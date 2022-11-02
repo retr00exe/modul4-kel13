bf753d065d0955e6421fc7964fe47d215be73c80
akbarfach <akbar.fach77@gmail.com>
2022-11-02 22:54:44 +0700

    useRef

---
+++ src/learn-useRef/index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <><div class="Main">
+        <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </div>
+      
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
